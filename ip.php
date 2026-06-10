281d22090372bc0c7901d6f01a684aff0f73fe09
latinoschats <laberinto692@gmail.com>
2026-06-09 20:32:09 -0400

    Update ip.php

--- ip.php
+++ ip.php
@@ -1,27 +1,22 @@
 <?php
  
-
-Sanchivo "data.txt"; //Nombre del archivo donde se almacenard la información.
+$archivo "data.txt"; //Nombre del archivo donde se almacenard la información.
  
 $d_ip "Direccion IP: (S SERVER["REMOTE_ADDR"}}\n"; //String con la dirección IP incluida.
  
-Sfecha "Fecha: ".date('D dS M,Y h:1 a')."\n\n";
+$fecha "Fecha: ".date('D dS M,Y h:1 a')."\n\n"; //String con la fecha y hora.
  
-//String con la fecha y hora.
+$texto $d_ip.$fecha; //String que se escribird en el archivo.
  
-Stexto Sd ip.sfecha;
- 
-//String que se escribird en el archivo.
- 
-//Quedard algo est:
+//Quedard algo asi:
  
 //Dirección IP: 127.0.0.1 (Para dar un ejemplo te ip tocol.)
  
 //Fecha: Fecha y hora en la cual el usuario entro a to url en su navegador.
  
-5th fopen(Sarchivo, 'a'); //Se abre el archivo con el nombre "data.txt".
+5th fopen($archivo, 'a'); //Se abre el archivo con el nombre "data.txt".
  
-fwrite($fh, Stexto); //Se guarda el contenido de la variable "texto" en el archivo.
+fwrite($fh, $texto); //Se guarda el contenido de la variable "texto" en el archivo.
  
 fclose($th); //Se cierra el archivo.
  
