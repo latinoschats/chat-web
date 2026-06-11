f4c2e99dd6dd1700af8aaebc0fd93cca76c8e4ff
latinoschats <laberinto692@gmail.com>
2026-06-11 18:59:42 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,9 @@
 <?php
-// Obtener la IP del visitante
 $ip = $_SERVER['REMOTE_ADDR'];
+$hora = date("Y-m-d H:i:s");
 
-// Mostrar la IP en la página
+// Guardar en visitas.txt
+file_put_contents("visitas.txt", $ip . " | " . $hora . "\n", FILE_APPEND);
+
 echo "Tu IP es: " . $ip;
 ?>
