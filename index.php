08b019bb1e3949598cf4887b6070101eab90af5d
latinoschats <laberinto692@gmail.com>
2026-07-02 18:38:00 -0400

    Delete index.php

--- index.php
+++
@@ -1,9 +0,0 @@
-<?php
-$ip = $_SERVER['REMOTE_ADDR'];
-$hora = date("Y-m-d H:i:s");
-
-// Guardar en visitas.txt
-file_put_contents("visitas.txt", $ip . " | " . $hora . "\n", FILE_APPEND);
-
-echo "Tu IP es: " . $ip;
-?>
